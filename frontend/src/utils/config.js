c6d483c4daf7693ab40c515fb0100aa49abb5076
Jayesh Khatri <khatrijayeshrl@gmail.com>
2024-05-11 04:27:49 +0530

    Some Final Changes

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,2 @@
-export const BASE_URL = 'https://final-year-project-wr90.onrender.com/api/v1'
-// export const BASE_URL = 'https://localhost:4000/api/v1'
+export const BASE_URL = 'http://localhost:4000/api/v1'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
